f8e4d2291297b3a6a02c73fc510cd5f26eb98d82
ChandraMohan-Sah <csah9628@gmail.com>
2025-06-07 15:08:29 +0545

    refactor : kept the django_installer. folder inside  into django_installer folder

--- my_django_starter/main.py
+++ my_django_starter/main.py
@@ -3,7 +3,7 @@ import json
 from my_django_starter.modules.banner.banner import Banner
 from my_django_starter.modules.os_detector.os_detector import OSDetector
 from my_django_starter.modules.virtualenv_creator.virtualenv_creator import VirtualEnvCreator
-from my_django_starter.modules.django_installer import DjangoInstaller
+from my_django_starter.modules.django_installer.django_installer import DjangoInstaller
 from my_django_starter.modules.project_creator import ProjectCreator
 from my_django_starter.modules.app_creator import AppCreator
 from my_django_starter.modules.settings_modifier import SettingsModifier
