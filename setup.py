9cb4fef89afd45a34e800826292b75aa4a7626f3
Chandra Mohan Sah <97285843+ChandraMohan-Sah@users.noreply.github.com>
2025-06-08 06:49:42 +0545

    chore: changed version number of setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="my-django-starter",
-    version="0.1.4",
+    version="0.1.5",
     author="Chandra Mohan Sah",
     author_email="csah9628@gmail.com",
     description="A starter kit to quickly scaffold Django projects.",
