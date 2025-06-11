cbc2b22e164c90a64112e7b4bc6f6ff61abf1166
ChandraMohan-Sah <csah9628@gmail.com>
2025-06-11 14:19:51 +0545

    changed the version number from 0.1.8 to 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="my-django-starter",
-    version="0.1.8",
+    version="0.1.9",
     author="Chandra Mohan Sah",
     author_email="csah9628@gmail.com",
     description="A starter kit to quickly scaffold Django projects.",
